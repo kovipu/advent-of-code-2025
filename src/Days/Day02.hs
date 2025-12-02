4f1edcdd6c2acf3013c9e9a0e18c5d56f598b9bc
Konsta Purtsi <konsta@purtsi.fi>
2025-12-02 09:26:13 +0200

    Day 2

--- src/Days/Day02.hs
+++ src/Days/Day02.hs
@@ -12,7 +12,7 @@ import qualified Data.Vector as Vec
 import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay, Day)
-import Data.Attoparsec.Text
+import Data.Attoparsec.Text (Parser, sepBy, decimal, char)
 import Data.Void
 {- ORMOLU_ENABLE -}
 
@@ -21,19 +21,53 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = parsePair `sepBy` char ','
+
+parsePair = do
+  a <- decimal
+  char '-'
+  b <- decimal
+  pure (a, b)
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [(Int, Int)]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = foldl fn 0
+
+fn acc (a, b) = foldl (+) acc $ filter isPasswordInvalid [a..b]
+
+isPasswordInvalid n = start == end
+  where
+    s = show n
+    l = length s
+    (start, end) = splitAt (l `div` 2) s
+
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = foldl fn' 0
+
+fn' acc (a, b) = foldl (+) acc $ filter isPasswordInvalid' [a..b]
+
+isPasswordInvalid' n = any (\k -> isChunkMatch k s) [1..(l - 1)]
+  where
+    s = show n
+    l = length s
+
+    -- do all of the members of that group match?
+    isChunkMatch k s = all (== head chunks) chunks
+      where
+        chunks = equalChunks k s
+
+    -- generate chunks with lengths 1..(length p)
+    equalChunks _ [] = []
+    equalChunks k xs = take k xs : equalChunks k (drop k xs)
+
+
+

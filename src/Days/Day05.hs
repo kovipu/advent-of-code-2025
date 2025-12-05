a3731baac01904c2897ad3d57d7934f5623641de
Konsta Purtsi <konsta@purtsi.fi>
2025-12-05 09:36:13 +0200

    Day 5

--- src/Days/Day05.hs
+++ src/Days/Day05.hs
@@ -20,20 +20,51 @@ runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
+data Input = Input
+  { ranges :: [(Int, Int)],
+    ingredients :: [Int]
+  }
+  deriving (Show)
+
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = do
+  ranges <- parseRange `sepBy` endOfLine
+  many1 space
+  ingredients <- decimal `sepBy` endOfLine
+  pure $ Input {ranges, ingredients}
 
------------- TYPES ------------
-type Input = Void
-
-type OutputA = Void
-
-type OutputB = Void
+parseRange = do
+  a <- decimal
+  char '-'
+  b <- decimal
+  pure (a, b)
 
 ------------ PART A ------------
-partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA :: Input -> Int
+partA Input {ranges, ingredients} =
+  foldl
+    ( \acc ig ->
+        if any (\(a, b) -> a <= ig && ig <= b) ranges
+          then acc + 1
+          else acc
+    )
+    0
+    ingredients
 
 ------------ PART B ------------
-partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB :: Input -> Int
+partB Input {ranges} = sum $ map (\(a, b) -> b - a + 1) $ mergeOverlapping ranges
+
+-- if any of the ranges overlap -> join them together -> recurse until no change
+mergeOverlapping ranges =
+  if ranges == ranges' then ranges' else mergeOverlapping ranges'
+  where
+    ranges' = foldl mergeRange [] ranges
+
+mergeRange [] r = [r]
+mergeRange ((x, y) : xs) (a, b) =
+  if max x a <= min y b
+    -- if ranges overlap -> combine to give the biggest range
+    then (min a x, max b y) : xs
+    -- otherwise tail recurse
+    else (x, y) : (mergeRange xs (a, b))

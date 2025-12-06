e61bf3e566c63baeac3283a257aa606ec6c0b1b9
Konsta Purtsi <konsta@purtsi.fi>
2025-12-06 12:57:41 +0200

    Day 6

--- src/Days/Day06.hs
+++ src/Days/Day06.hs
@@ -1,7 +1,11 @@
 module Days.Day06 (runDay) where
 
 {- ORMOLU_DISABLE -}
+import Control.Applicative ((<|>), many, optional)
+import           Data.Functor            (($>))
 import Data.List
+import Data.List.Split (splitWhen)
+import Data.Char
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import Data.Maybe
@@ -20,20 +24,45 @@ runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
+type Input = ([String], [Op])
+
+data Op = Add | Mul deriving (Show, Eq)
+
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
-
------------- TYPES ------------
-type Input = Void
-
-type OutputA = Void
-
-type OutputB = Void
+inputParser = do
+  nums <- count 4 parseLine
+  ops <- parseOp `sepBy` many' space
+  pure (nums, ops)
+  where
+    parseOp = char '*' $> Mul <|> char '+' $> Add
+    parseLine = do
+      ts <- many1 $ notChar '\n'
+      endOfLine
+      pure ts
 
 ------------ PART A ------------
-partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+-- Changed the Input data & Parser for part B so the part A code broke.
+partA :: Input -> Int
+partA inp = 0
+
+-- partA (nums, ops) =
+--   foldl
+--     ( \acc (op, ns) ->
+--         if op == Mul
+--           then acc + foldl (*) 1 ns
+--           else acc + sum ns
+--     )
+--     0
+--     (zip ops $ transpose nums)
 
 ------------ PART B ------------
-partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB :: Input -> Int
+partB (nums, ops) =
+  foldl
+    ( \acc (op, ns) ->
+        if op == Mul
+          then acc + foldl (\acc n -> (read n) * acc) 1 ns
+          else acc + foldl (\acc n -> (read n) + acc) 0 ns
+    )
+    0
+    (zip ops (splitWhen (== "    ") $ transpose nums))
